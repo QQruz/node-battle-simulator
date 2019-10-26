a9260faa971ecbdd6e14aa09138775073c382701
QQruz <bbojan@protonmail.com>
2019-10-26 11:49:47 +0200

    Add db class

---
+++ src/utils/db.js
@@ -0,0 +1,34 @@
+require('dotenv').config(); // TOREMOVE: after index is added
+const { MongoClient } = require('mongodb');
+
+module.exports = class DB {
+  static async connect() {
+    const options = { useUnifiedTopology: true };
+
+    let url = 'mongodb://';
+
+    if (process.env.DB_USER) {
+      url += `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`;
+    }
+
+    url += `${process.env.DB_HOST}:${process.env.DB_PORT}`;
+
+    const client = new MongoClient(url, options);
+
+    let connection;
+
+    try {
+      connection = await client.connect();
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+
+    DB.connection = connection.db(process.env.DB_NAME);
+    console.log(`Connected to db: ${process.env.DB_NAME}`);
+  }
+
+  static get() {
+    return DB.connection;
+  }
+};
